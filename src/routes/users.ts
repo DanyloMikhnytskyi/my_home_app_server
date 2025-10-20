b42dad66c1e448704c9d65b26aa9c2b32bec65fb
Danylo Mikhnytskyi <@danilmickhnitskyy>
2025-10-20 11:33:10 +0200

    feat: initial project setup with Express, TypeScript, ESLint and Prettier

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/users', (_req: Request, res: Response) => {
-  res.send('...345...123. 34.');
+  res.send('...345...123. 34  .');
 });
 
 export const usersRouter = router;
