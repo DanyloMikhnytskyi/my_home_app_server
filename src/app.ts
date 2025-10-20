00e9e1a0e7d1d0d9bb8765bb5170989a7fe6ae91
Danylo Mikhnytskyi <@danilmickhnitskyy>
2025-10-20 16:15:09 +0200

    feat: Fix users.ts

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
 import { usersRouter } from './routes/users.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -17,5 +16,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/api', usersRouter);
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,8 @@
 import express, { Request, Response } from 'express';
 
-const router = express.Router();
+export const usersRouter = express.Router();
 
 /* GET users listing. */
-router.get('/users', (_req: Request, res: Response) => {
+usersRouter.get('/users', (_req: Request, res: Response) => {
   res.send('...345...123. 34  .');
 });
-
-export const usersRouter = router;
